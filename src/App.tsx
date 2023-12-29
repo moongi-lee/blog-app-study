ac2dee0ffb9c666c4e3e481d08774d8d85eb5583
leemoongi <111174446+moongi-lee@users.noreply.github.com>
2023-12-30 05:15:41 +0900

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes, Navigate, Link} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/posts">Post List</Link>
+        </li>
+        <li>
+          <Link to="/posts/:id">Post Detail</Link>
+        </li>
+        <li>
+          <Link to="/posts/new">Post New</Link>
+        </li>
+        <li>
+          <Link to="/posts/edit/:id">Post Edit</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>}/>
+        <Route path="/posts" element={<div>Post List Page</div>}/>
+        <Route path="/posts/:id" element={<div>Post Detail Page</div>}/>
+        <Route path="/posts/new" element={<div>Post New Page</div>}/>
+        <Route path="/posts/edit/:id" element={<div>Post Edit Page</div>}/>
+        <Route path="/profile" element={<div>Profile Page</div>}/>
+        <Route path="*" element={<Navigate replace to="/"/>}/>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
