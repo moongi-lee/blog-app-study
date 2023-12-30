840bc6179fdc41c5c62e0cf19ed84d18ba29dc73
leemoongi <111174446+moongi-lee@users.noreply.github.com>
2023-12-31 03:16:48 +0900

    layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,24 +19,27 @@ export default function Home() {
         </div>
         <div className="post__list">
           {
-            [...Array(10)].map((e,index)=>(
-                <div key={index} className="post__box" >
-                  <Link to={`/posts/${index}`}>
-                    <div className="post__profile-box">
-                      <div className="post__profile"/>
-                      <div className="post__author-name">박지봉</div>
-                      <div className="post__date">2023.07.08 토요일</div>
-                    </div>
-                    <div className="post__title">게시글 {index}</div>
-                    <div className="post__text">안녕하세요 저는 이문기 친구 박지봉 입니다.</div>
-                    <div className="post__utills-box">
-                      <div className="post__delete">삭제</div>
-                      <div className="post__edit">수정</div>
-                    </div>
-                  </Link>
-                </div>
-            ))
+              [...Array(10)].map((e,index)=>(
+                  <div key={index} className="post__box" >
+                    <Link to={`/posts/${index}`}>
+                      <div className="post__profile-box">
+                        <div className="post__profile"/>
+                        <div className="post__author-name">박지봉</div>
+                        <div className="post__date">2023.07.08 토요일</div>
+                      </div>
+                      <div className="post__title">게시글 {index}</div>
+                      <div className="post__text">안녕하세요 저는 이문기 친구 박지봉 입니다.</div>
+                      <div className="post__utills-box">
+                        <div className="post__delete">삭제</div>
+                        <div className="post__edit">수정</div>
+                      </div>
+                    </Link>
+                  </div>
+              ))
           }
+        </div>
+        <div>
+
         </div>
         <footer>
           <Link to="/posts/new">글쓰기</Link>
